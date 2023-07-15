db4be8d01d5002fe8d7c30ba3f54c4067f8394ad
YosukeKato <katouyousuke@YosukeKato.local>
2023-07-15 20:01:33 +0900

    add:prettier

--- src/app/_components/counter/logics.ts
+++ src/app/_components/counter/logics.ts
@@ -1,10 +1,10 @@
-import {useState} from "react";
+import { useState } from "react";
 
 export const useCounter = () => {
-    const[count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-    return {
-        count,
-        setCount
-    }
-}
+  return {
+    count,
+    setCount,
+  };
+};
